65685c0dcb3c6d04bb309df1ad1f0688600874c8
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2022-03-31 13:01:38 +0300

    React queries

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { QueryClient, QueryClientProvider} from 'react-query'
+import Posts from './components/Posts';
 import './App.css';
+
+
+// TODO: Check on why its not displaying content on the browser
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+       <Posts />
+    </QueryClientProvider>
   );
 }
 
